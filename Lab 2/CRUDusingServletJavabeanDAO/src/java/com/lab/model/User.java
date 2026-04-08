59130a2a44a1bc49e12080eaafa86e6660de728c
wcaiyi05-SE <wcaiyi05@gmail.com>
2026-04-08 22:53:49 +0800

    lab 2

---
+++ Lab 2/CRUDusingServletJavabeanDAO/src/java/com/lab/model/User.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lab.model;
+
+/**
+ *
+ * @author Asus
+ */
+public class User {
+    
+    private int id;
+    private String username;
+    private String password;
+    private String roles;
+    
+    public User(){
+        
+    }
+    
+    public User(int id, String username, String password, String roles){
+        this.id = id;
+        this.password = password;
+        this.roles = roles;
+        this.username = username;
+    }
+    
+    public User(String username, String password, String roles){
+        this.password = password;
+        this.roles = roles;
+        this.username = username;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+    
+}
